49bfa47041dda05c6c8196b4d3a81859ad130955
LulaPadberg <155753587+LulaPadberg@users.noreply.github.com>
2025-07-11 07:48:34 +0800

    feat: add new features (15 files)

---
+++ next.config.js
@@ -0,0 +1,23 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  images: {
+    domains: ['ipfs.io'],
+  },
+  webpack: (config) => {
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      net: false,
+      tls: false,
+    };
+    return config;
+  },
+};
+
+module.exports = nextConfig;
